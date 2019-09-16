7f9c6662a8ebfe86cc176b002384dc3867dbbe08
weihanLiu <liu.weih@husky.neu.edu>
2019-09-16 16:41:37 -0400

    hw02 done v1.0

--- calc.js
+++ calc.js
@@ -4,10 +4,11 @@
 	var current = "0";
 	var result = "0";
 	var mode = "input";
-	var pre_operator = "";
+	var pre_operator = ""; //store the previous operator before enter an number 
 
 	function updateDisplay(n) {
-		//if integer larger than display width, show ""
+		//if integer larger than max display width(12), show 
+		//"out of Bound"
 		if (n.length > 12 && !n.includes(".")) {
 			n = "Out Of Bound";
 		}
@@ -31,14 +32,14 @@
 		//end of display
 		if (current.length < 12) {
 			var n = b.value; //value is string
-			if (mode === "input") {
+			if (mode === "input") { //previous clicked is number
 				if(current !== "0") {
 					current = current + n;
 				} else {
 					current = n;
 				}
 			}
-			else {
+			else {//previous clicked is operator
 				mode = "input";
 				current = n;
 				}
@@ -49,6 +50,7 @@
 
 	function operatorClick(b) {
 		b.onclick = function() {
+			/*previous clicked is number, do calculation using stored previous operator*/
 			if (mode === "input") {
 				switch(pre_operator) {
 				case "+/=": 
@@ -66,18 +68,22 @@
 				case "":
 					result = current;
 				}
-				
+				/*after calculation, show result and reset current and mode, store the
+				 * newly clicked operator for later calculation process*/
 				updateDisplay(result);
 				current = "0";
 				mode = "operate";
 				pre_operator = b.value;
 			}
+			/*if previous clicked is operaton, update the store operator(pre_operator)
+			 * to the newly clicked one and do nothing to the result number*/
 			else {
 				pre_operator = b.value;
 			}
 		}
 	}
 
+	/*reset everything to initial value*/
 	function clearClick() {
 		result = "0";
 		current = "0";
@@ -86,6 +92,7 @@
 	}
 
 
+	/*add corresponding eventlistener to different buttons*/
 	function init() {
 		var number = document.getElementsByClassName("number");
 		// number = Array.from(number);
@@ -104,6 +111,9 @@
 		}
 	}
 
+	/*Delay the setup code until page is fully loaded.
+	 * Citied from lecture code: http://khoury.neu.edu/~ntuck/courses/
+	 * 2019/09/cs5610/notes/02-browsers/page/code.js*/	
 	window.addEventListener("load", init, false);
 
 })();
